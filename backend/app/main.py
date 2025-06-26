b2698619fc4dec100b62fd14e9cc4ca40dcfdd33
GSinghh <50472056+GSinghh@users.noreply.github.com>
2025-06-25 19:45:44 -0700

    moved api endpoints to individual files

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,26 +1,19 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.api.trades import router as trades_router
 
 origins = [
     "http://localhost:3000",
 ]
 
-journal = FastAPI()
+app = FastAPI()
 
-journal.add_middleware(
+app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,            # or ["*"] to allow all
+    allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["*"],              # GET, POST, PUT, DELETE, etc.
-    allow_headers=["*"],              # Content-Type, Authorization, etc.
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@journal.get("/api/test")
-async def testing():
-    return {"Message":"Endpoint is Active"}
-
-@journal.post("/api/file_upload")
-async def parse_csv(file: UploadFile = File(...)):
-    if not file:
-        return {"Message":"File Not Found"}
-    return {"File Name": f"{file.filename}"}
+app.include_router(trades_router, prefix="/trades")
